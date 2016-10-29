858bc7944b5f8dd10414a5b287dc6931241c1169
gilbertjacke <gilbertjacke@gmail.com>
2016-10-29 19:05:13 +0100

    get request

---
+++ spec/index.js
@@ -0,0 +1,13 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const {expect} = require('chai');
+
+
+describe('#get', function (){
+  it('responds with 200 and retrives the correct recording', function(done){
+    request(server)
+    .get(2016-10-29/Accounts/`${AC404c82882a45b7b15080a3c41a5cfcc0}`/Recordings/`${RE12410d30bf4624370ce014dd144e3264}`);
+    .expect(function(response){
+    });
+  });
+});
