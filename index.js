c23e772d19bebf875e649cb57b0fa686fc7e3b1b
gilbertjacke <gilbertjacke@gmail.com>
2016-10-29 15:11:11 +0100

    make a phone call get xml

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const twilio = require('twilio');
+
+let app = express();
+
+// Returns TwiML which prompts the caller to record a message
+app.post('/record', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
+  twiml.say('Hello, thanks for calling Julie. What are you interested in today?');
+
+  console.log('***');
+  console.log(response);
+  // Use <Record> to record the caller's message
+  twiml.record();
+
+  // End the call with <Hangup>
+  twiml.hangup();
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+// Create an HTTP server and listen for requests on port 3000
+app.listen(3000)
+  console.log('App is listening on 3000');
