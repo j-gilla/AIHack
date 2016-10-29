b5922c5222e851b4229ba741fac58508689cecd2
Paul Richards <peedeerich@gmail.com>
2016-10-29 22:32:14 +0100

    get keywords from string

---
+++ hobbies.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var _ = require('underscore');
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+
+var hobbies = JSON.parse(fs.readFileSync('./hobbies.json'), 'utf8');
+
+hobbies = _.map(hobbies.query.pages['31257416'].links, function (el) {
+  return el.title;
+});
+
+
+
+
+console.log(hobbies);
