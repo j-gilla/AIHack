52db7ac12dc7cdac8d5b1a081721ccb1c6912a3c
gilbertjacke <gilbertjacke@gmail.com>
2016-10-30 00:53:22 +0100

    can get last transcript

--- record.js
+++ record.js
@@ -10,15 +10,19 @@ const client = require('twilio')(accountSID, authToken);
 //         //console.log(data.transcriptionText);
 //     });
 // });
+//
+// client.recordings.list({ dateCreated: "2016-10-30" }, function(err, data) {
+//     data.recordings.forEach(function(recording) {
+//         console.log(data);
+//     });
+// });
 
-client.recordings.list({ dateCreated: "2016-10-30" }, function(err, data) {
-    data.recordings.forEach(function(recording) {
-        console.log(data);
-    });
+client.transcriptions("TRb2d7339d66e57918aa04c1f6959cde63").get(function(err, transcription) {
+    console.log(transcription.transcriptionText);
 });
 
 //Get all working
 //from this extract the latest, first one in the object
-//extract the latest time sig
+//extract the latest time sigx
 //replace transcriptions arg
 //with data from json
